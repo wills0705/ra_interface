1d6156da56f3a12ed570a016abb9c2e3c0c99007
董文涵 <dongwenhan@Wenhan-MacBook-Pro.local>
2025-07-01 16:47:22 -0400

    Allow Render domain in Vite preview config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import {AntDesignVueResolver} from 'unplugin-vue-components/resolvers'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Components({
-    resolvers:[AntDesignVueResolver({
-      importStyle:false,
-    })]
-  })],
-  resolve:{
-    extensions:['.mjs','.js','.json','.vue']
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [AntDesignVueResolver({
+        importStyle: false,
+      })]
+    })
+  ],
+  resolve: {
+    extensions: ['.mjs', '.js', '.json', '.vue']
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: ['ra-interface.onrender.com']
   }
 })
